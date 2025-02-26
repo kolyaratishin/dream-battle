1383a732746c9afa1d14de41b68cd793e072bab4
Mykola Ratyshyn <mykola.ratyshyn@eleks.com>
2025-02-26 13:57:26 +0200

    project init

---
+++ src/main/java/com/dream/battle/DreamBattleApplication.java
@@ -0,0 +1,13 @@
+package com.dream.battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamBattleApplication.class, args);
+	}
+
+}
